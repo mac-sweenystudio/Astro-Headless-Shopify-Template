85f5afa8b0011a445f6447c6fcf315270b0468a3
Mac Sweeny <macsweenydev@gmail.com>
2024-01-29 11:25:00 +1000

    Update Astro configuration and package.json
    This commit updates the Astro configuration file (astro.config.mjs) to include the "vercel" adapter and the "svelte" integration. It also updates the package.json file to use the latest versions of Astro and its dependencies.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,18 @@
 import { defineConfig } from "astro/config";
+import svelte from "@astrojs/svelte";
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
-import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
   adapter: vercel(),
-  integrations: [
-    tailwind({
-      config: {
-        applyBaseStyles: false,
-      },
-    }),
-    svelte(),
-  ],
+  output: "server",
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), svelte()]
 });
